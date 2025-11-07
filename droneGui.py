a2c476482f519761d654f7dd1a38eea3b796ec48
Nir12212 <niry.3d@gmail.com>
2025-11-07 19:21:16 +0200

    there is a stream option

--- droneGui.py
+++ droneGui.py
@@ -8,11 +8,16 @@ from kivy.uix.image import Image
 import socket
 import json
 import time
- 
+import struct
+from PIL import Image as PILImage
+import io
+import threading
+from kivy.clock import Clock
 
 ESP_IP = "192.168.4.1"
 DATA_PORT = 1234
 PIC_PORT = 1235
+STREAM_PORT = 1236
 #MAIN MENU
 class MainMenu(Screen):
     def __init__(self, **kwargs):
@@ -35,14 +40,25 @@ class MainMenu(Screen):
             color=(1, 1, 1, 1),
             background_color=(0, 0.5, 1, 1)
         )
-        
+        streamBtn = Button(
+            text="STREAM",
+            size_hint=(None, None),
+            size=(150, 80),
+            font_size=20,
+            color=(1, 1, 1, 1),
+            background_color=(0, 0.5, 1, 1)
+        )
         picBtn.bind(on_press=self.goShowPicsPage)
         dataBtn.bind(on_press=self.goDataPage)
-        
+        streamBtn.bind(on_press=self.goStreamPage)
+
         layout.add_widget(dataBtn)
         layout.add_widget(picBtn)
+        layout.add_widget(streamBtn)
         self.add_widget(layout)
 
+    def goStreamPage(self,instance):
+        self.manager.current = "stream"
     def goDataPage(self, instance):
         self.manager.current = "data"
 
@@ -210,18 +226,95 @@ class PicturesPage(Screen):
         self.manager.current = "menu"
 
 
-        
-            
+class StreamPage(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout = FloatLayout()
 
-#  APP 
-class droneGui(App):
+        # Image widget
+        self.stream_image = Image(
+            allow_stretch=False,
+            keep_ratio=True,
+            size_hint=(0.8, 0.8),
+            pos_hint={'x': 0.1, 'y': 0.15}
+        )
+
+        # Start button
+        startBtn = Button(
+            text="start",
+            size_hint=(None, None),
+            size=(200, 80),
+            pos_hint={'x': 0.60, 'y': 0.05},
+            background_color=(0, 0.5, 1, 1)
+        )
+
+        # Back button
+        backBtn = Button(
+            text="back",
+            size_hint=(None, None),
+            size=(70, 80),
+            pos_hint={'x': 0.10, 'y': 0.05},
+            background_color=(0, 0.5, 1, 1)
+        )
+
+        # Bind buttons
+        backBtn.bind(on_press=self.goBack)
+        startBtn.bind(on_press=self.start_streaming)
+
+        # Add widgets
+        self.layout.add_widget(startBtn)
+        self.layout.add_widget(backBtn)
+        self.layout.add_widget(self.stream_image)
+        self.add_widget(self.layout)
+
+    def goBack(self, instance):
+        self.manager.current = "menu"
+
+    def start_streaming(self, instance):
+        # Run streaming in a separate thread
+        threading.Thread(target=self.stream_thread, daemon=True).start()
+
+    def stream_thread(self):
+        try:
+            s = socket.socket()
+            s.connect((ESP_IP, STREAM_PORT))
+            while True:
+                size_data = s.recv(4)
+                if not size_data:
+                    break
+                size = struct.unpack("<L", size_data)[0]
+
+                buf = b""
+                while len(buf) < size:
+                    buf += s.recv(size - len(buf))
+
+                filename = "temp_stream.jpg"
+                with open(filename, "wb") as f:
+                    f.write(buf)
+
+                # Schedule update on the main thread
+                Clock.schedule_once(lambda dt, fn=filename: self.update_image(fn))
+        except Exception as e:
+            print("Stream error:", e)
+        finally:
+            s.close()
+
+    def update_image(self, filename):
+        self.stream_image.source = filename
+        self.stream_image.reload()
+
+
+            
+class DroneGui(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MainMenu(name="menu"))
         sm.add_widget(DataPage(name="data"))
         sm.add_widget(PicturesPage(name="pics"))
+        sm.add_widget(StreamPage(name="stream"))
+
         return sm
 
 #  RUN 
 if __name__ == "__main__":
-    droneGui().run()
+    DroneGui().run()
